429a86bd989630c42992e9f358937b41d5b4e82e
changj35513 <changj35513@gmail.com>
2022-11-17 04:42:54 +0000

    finished helper methods video 2 i think except for director

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,10 +24,11 @@ class MoviesController < ApplicationController
   end
 
   def create
+    movie_attributes = params.require(:movie).permit(:title, :description)
     # @movie = params.require(:movie).permit(:title, :description)
-    @movie = Movie.new
-    @movie.title = params.fetch(:movie).fetch(:title)
-    @movie.description = params.fetch(:movie).fetch(:description)
+    @movie = Movie.new(movie_attributes)
+    # @movie.title = params.fetch(:movie).fetch(:title)
+    # @movie.description = params.fetch(:movie).fetch(:description)
 
     if @movie.valid?
       @movie.save
@@ -42,17 +43,18 @@ class MoviesController < ApplicationController
   end
 
   def update
-    
-    movie = Movie.find(params.fetch(:id))
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.find(params.fetch(:id))
 
-    movie.title = params.fetch("title")
-    movie.description = params.fetch("description")
+    @movie.update(movie_attributes)
+    # movie.title = params.fetch("title")
+    # movie.description = params.fetch("description")
 
-    if movie.valid?
-      movie.save
-      redirect_to(movie_url(movie), notice: "Movie updated successfully.")
+    if @movie.valid?
+      @movie.save
+      redirect_to(movie_url(@movie), notice: "Movie updated successfully.")
     else
-      redirect_to(movie_url(movie), alert: "Movie failed to update successfully.")
+      redirect_to(movie_url(@movie), alert: "Movie failed to update successfully.")
     end
   end
 
